4d6ce13858d271d4e4e6d0ddc356a95369b2c3d3
Evan Hong <evangelurtz@gmail.com>
2020-01-14 15:56:24 +0900

    Add player movement clamp and wrap

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,16 +10,31 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Take the current position = new position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement() 
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
