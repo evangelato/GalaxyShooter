035b2e77a460c4adfda70cb46b4ea346fd442b00
Evan Hong <evangelurtz@gmail.com>
2020-01-22 23:57:29 +0900

    Add asteroid and its behaviors

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotateSpeed = 3.0f;
+    [SerializeField]
+    private GameObject _explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0f, 0f, _rotateSpeed * Time.deltaTime, Space.Self);
+    }
+
+    // Check for laser collision (Trigger)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {      
+            GameObject asteroid = Instantiate(_explosion, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject, 0.2f);
+        }
+    }
+}
